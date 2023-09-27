cd522b8b9315fac5fd62667e12a55b69ba1a72b7
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2023-09-28 00:34:46 +0300

    fix gameover date

--- src/game/observables/gameover.ts
+++ src/game/observables/gameover.ts
@@ -7,7 +7,7 @@ const CREDIT_GOAL = 300;
 export function createGameover(credits$: Credits, date$: Observable<Date>) {
     return combineLatest([
         credits$.pipe(map(credits => credits > CREDIT_GOAL)),
-        date$.pipe(map(date => isEqual(date, new Date(2023, 8, 31)))),
+        date$.pipe(map(date => isEqual(date, new Date(2028, 8, 31)))),
     ]).pipe(
         map(conditions => conditions.some(condition => condition)),
         share(),
