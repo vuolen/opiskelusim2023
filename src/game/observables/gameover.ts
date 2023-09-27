9d2e1b0149bf016f47cf2969c93c4b5ac277ba36
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2023-09-28 00:37:03 +0300

    fix gameover again

--- src/game/observables/gameover.ts
+++ src/game/observables/gameover.ts
@@ -1,13 +1,13 @@
 import { Observable, combineLatest, map, share, startWith } from "rxjs";
 import type { Credits } from "./credits";
-import { isEqual } from "date-fns";
+import { isAfter } from "date-fns";
 
 const CREDIT_GOAL = 300;
 
 export function createGameover(credits$: Credits, date$: Observable<Date>) {
     return combineLatest([
         credits$.pipe(map(credits => credits > CREDIT_GOAL)),
-        date$.pipe(map(date => isEqual(date, new Date(2028, 8, 31)))),
+        date$.pipe(map(date => isAfter(date, new Date(2028, 8, 31)))),
     ]).pipe(
         map(conditions => conditions.some(condition => condition)),
         share(),
